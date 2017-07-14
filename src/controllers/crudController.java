4d1ed54918db346e2b4e5e538fbbdb15e4a9a8d1
TravisWay <travis_sti@yahoo.com>
2017-07-14 15:28:29 -0600

    13 steps complete

---
+++ src/controllers/crudController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class crudController {
+
+	//example
+	@RequestMapping("route.do")
+	  public ModelAndView processData(@RequestParam("data") String s) {
+	    String allCaps = s.toUpperCase();
+	    ModelAndView mv = new ModelAndView();
+	    mv.setViewName("view.jsp");
+	    mv.addObject("result", allCaps);
+	    return mv;
+	  }
+	}
+
